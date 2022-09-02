6dd45dbf177be51e82d591c7cb433a1d5895f12e
yaoxinyu <894742882@qq.com>
2022-09-02 17:59:24 +0800

    -

---
+++ scripts/config/webpack.common.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const { isDev, PROJECT_PATH } = require('../constant')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  entry: {
+    app: path.resolve(PROJECT_PATH, './src/app.js'),
+  },
+  output: {
+    filename: `js/[name]${isDev ? '' : '.[hash:8]'}.js`,
+    path: path.resolve(PROJECT_PATH, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(PROJECT_PATH, './public/index.html'),
+      filename: 'index.html',
+      cache: false, // 特别重要：防止之后使用v6版本 copy-webpack-plugin 时代码修改一刷新页面为空问题。
+      minify: isDev
+        ? false
+        : {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            removeComments: true,
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            minifyCSS: true,
+            minifyJS: true,
+            minifyURLs: true,
+            useShortDoctype: true,
+          },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false, // 默认就是 false, 若要开启，可在官网具体查看可配置项
+              sourceMap: isDev, // 开启后与 devtool 设置一致, 开发环境开启，生产环境关闭
+              importLoaders: 1, // 指定在 CSS loader 处理前使用的 laoder 数量
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: isDev,
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
---
+++ src/app.js
@@ -0,0 +1,6 @@
+import './app.css'
+
+const root = document.querySelector('#root')
+root.innerHTML = 'hello, webpack!'
+
+const a = 5
